f56495cd4c8df598cdace862d6678e6f1406c67f
eduardo_dorado <eduardo.dorado@sanmina.com>
2023-03-31 18:11:19 -0600

    update - navbar items added to an array

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface NavItem {
+  title:  string;
+  url:   string;
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [`
+    li:hover {
+      cursor: pointer;
+    }
+  `]
 })
 export class NavbarComponent implements OnInit {
+
+  navItems: NavItem[] = [
+    { title:'Home',     url: '/' },
+    { title:'Projects', url: 'projects' },
+    { title:'Forms',    url: 'forms' },
+    { title:'Charts',   url: 'charts' },
+    { title:'Products', url: 'products' },
+    { title:'Tags',     url: 'tags' },
+  ];
 
   constructor() { }
 
